039dc333cf59dee6adb60919e2b5dd3fd14c721f
JoaoGoncaloFarinha <joaofarinha1994@gmail.com>
2018-07-23 22:11:53 +0100

    added logo with tilt library

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navigation/Navigation.js';
+import Logo from './components/logo/Logo.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/*<Logo />
+        <Logo />
+        {/*
         <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
