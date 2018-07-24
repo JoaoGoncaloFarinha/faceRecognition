40a55790204f51929db1fd879755ada2c8476b07
JoaoGoncaloFarinha <joaofarinha1994@gmail.com>
2018-07-24 21:44:56 +0100

    added dynamic background using react-particles

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,16 +1,91 @@
 import React, { Component } from 'react';
-import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation.js';
-import Logo from './components/logo/Logo.js'
-import ImageLinkForm from './components/imagelinkform/ImageLinkForm.js'
+import Logo from './components/logo/Logo.js';
+import Rank from './components/rank/Rank.js';
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm.js';
+import './App.css';
 
+const particleOptions = { particles: {
+    "number": {
+      "value": 350,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": true
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+        params={particleOptions}/>
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*
         <FaceRecognition /> */}
---
+++ facerecognition/src/components/rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rank = () => {
+	return(
+	<div>
+		<div className='white f3'>
+			<p> Your current rank is ... </p>
+			<h2>#5</h2>
+		</div>
+	</div>
+	);
+}
+
+export default Rank;
